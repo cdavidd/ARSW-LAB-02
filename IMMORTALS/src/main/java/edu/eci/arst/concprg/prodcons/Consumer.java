435d14e8a4e9e444b56bcc08eed231f6dff93492
unknown <2133561@labinfo.is.escuelaing.edu.co>
2019-08-21 08:26:08 -0500

    parte 1 p2

--- IMMORTALS/src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
+++ IMMORTALS/src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
@@ -6,6 +6,8 @@
 package edu.eci.arst.concprg.prodcons;
 
 import java.util.Queue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -23,12 +25,17 @@ public class Consumer extends Thread{
     @Override
     public void run() {
         while (true) {
-
-            if (queue.size() > 0) {
+            synchronized(queue){
+                while (queue.isEmpty()) {
+                    try {
+                        queue.wait();
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Consumer.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
                 int elem=queue.poll();
-                System.out.println("Consumer consumes "+elem);                                
+                System.out.println("Consumer consumes "+elem);
             }
-            
         }
     }
 }
